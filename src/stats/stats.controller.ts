e77fe38a887fbc1bdc7c7b497b5cbcab8e483f3e
smw0807 <smw0807@gmail.com>
2025-06-21 23:08:40 +0900

    스웨거

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { PlatformType } from '@/constants/platform';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('stats')
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
 
   @Get('rank')
+  @ApiOperation({ summary: '랭크 스탯 조회' })
+  @ApiQuery({
+    name: 'platform',
+    type: String,
+    example: 'kakao',
+    required: true,
+  })
+  @ApiQuery({ name: 'nickname', type: String, required: true })
   async getRankStats(
     @Query('platform') platform: PlatformType,
     @Query('nickname') nickname: string,
@@ -15,6 +24,14 @@ export class StatsController {
   }
 
   @Get('normal')
+  @ApiOperation({ summary: '일반 스탯 조회' })
+  @ApiQuery({
+    name: 'platform',
+    type: String,
+    example: 'kakao',
+    required: true,
+  })
+  @ApiQuery({ name: 'nickname', type: String, required: true })
   async getNormalStats(
     @Query('platform') platform: PlatformType,
     @Query('nickname') nickname: string,
