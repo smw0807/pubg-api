ee3e7287f7fd4f21780a4cb586bb0c4566768929
smw0807 <smw0807@gmail.com>
2025-06-22 12:19:19 +0900

    매치 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PlayersModule } from './players/players.module';
 import { PubgModule } from './pubg/pubg.module';
 import { SeasonsModule } from './seasons/seasons.module';
 import { StatsModule } from './stats/stats.module';
+import { MatchesModule } from './matches/matches.module';
 
 @Module({
-  imports: [ConfigModule, PlayersModule, PubgModule, SeasonsModule, StatsModule],
+  imports: [ConfigModule, PlayersModule, PubgModule, SeasonsModule, StatsModule, MatchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
