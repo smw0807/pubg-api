62c2767c772c067dc6d10a8a43cdbfc5a413633d
smw0807 <smw0807@gmail.com>
2026-05-17 17:19:58 +0900

    feat: health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @ApiTags('app')
 @Controller()
@@ -22,5 +23,21 @@ export class AppController {
   getHello(): string {
     const pubgApiUrl = this.configService.get<string>('pubg.apiUrl');
     return `PUBG API URL: ${pubgApiUrl}`;
+  }
+
+  @Get('health')
+  @ApiOperation({
+    summary: 'API 상태 확인',
+    description: 'API의 상태를 확인합니다.',
+  })
+  @ApiOkResponse({
+    description: 'API 상태 확인 성공',
+    schema: {
+      type: 'string',
+      example: 'API is running',
+    },
+  })
+  healthCheck(@Res() res: Response): Response {
+    return res.status(200).json({ message: 'API is running' });
   }
 }
